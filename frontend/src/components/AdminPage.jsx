7419120bf531ee73471bc4f5d63e2b04bf98a989
Wes Wolf <wesleywwolf@gmail.com>
2023-11-22 13:58:34 -0800

    added more dummy content to AdminPage.

--- frontend/src/components/AdminPage.jsx
+++ frontend/src/components/AdminPage.jsx
@@ -1,22 +1,62 @@
+
 import React from "react";
+import { Row, Col } from "react-bootstrap"; // Make sure to import Bootstrap components
 
 function AdminPage() {
+  // Dummy programs
+  const dummyPrograms = [
+    { id: 1, name: "LSAMP" },
+    { id: 2, name: "EWX" },
+    { id: 3, name: "ACCESS" },
+    // Add more dummy programs as needed
+  ];
+
+  // Dummy tags
+  const dummyTags = [
+    { id: 1, name: "STEM" },
+    { id: 2, name: "Engineering" },
+    { id: 3, name: "Low Income" },
+    // Add more dummy tags as needed
+  ];
+
   return (
     <div className="m-auto">
-      <div className="">
-        <header>
-          <h1 className="welcome text-primary text-center p-5">
-            Admin Page: add, remove, modify programs and tags
-          </h1>
-        </header>
-        <main>
+      {/* Programs section */}
+      <Row className="img-hero-welcome">
+        {/*<Col className="hero-img"></Col>*/}
+        <Col>
           <section>
-            <p className="intro text-center fs-5 fw-bold">
-              Here is where you can add some content
-            </p>
+            <h1 className="intro text-center fw-bold">
+              Admin Tools
+            </h1>
+            <p className = 'text-center'>Follow the instructions below to use the tool correctly..</p>
           </section>
-        </main>
-      </div>
+        </Col>
+      </Row>
+
+      {/* Tags section */}
+      <Row ClassName="mt-5">
+        <Col md={12} lg={6} className="border-end rounded-shadow" style={{ minHeight: "200px" }}>
+          <div className="text-center">
+            <h2>Programs</h2>
+              {dummyPrograms.map((program) => (
+                  <button>{program.name}</button>
+              ))}
+          </div>
+        </Col>
+
+        {/* Programs Section */}
+        <Col md={12} lg={6} className="border-end rounded-shadow" style={{minHeight: "200px"}}>
+          {/* You may replace this with your actual content */}
+          <div className="text-center">
+            <h2>Tags</h2>
+              {dummyTags.map((tag) => (
+                  <button>{tag.name}</button>
+              ))}
+          </div>
+        </Col>
+
+      </Row>
     </div>
   );
 }
