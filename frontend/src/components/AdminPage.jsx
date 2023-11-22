429e40b751ecfc38354257d045c842cd791667e9
Wes Wolf <wesleywwolf@gmail.com>
2023-11-22 15:17:15 -0800

    Added more content and styles to Admin Tools Page

--- frontend/src/components/AdminPage.jsx
+++ frontend/src/components/AdminPage.jsx
@@ -8,6 +8,18 @@ function AdminPage() {
     { id: 1, name: "LSAMP" },
     { id: 2, name: "EWX" },
     { id: 3, name: "ACCESS" },
+    { id: 4, name: "NATIONS" },
+    { id: 5, name: "GANAS" },
+    { id: 6, name: "EMPOWER" },
+    { id: 7, name: "TRIO" },
+    { id: 8, name: "S^3" },
+    { id: 9, name: "MESA C2C" },
+    { id: 11, name: "NSF REU"},
+    { id: 9, name: "URMP" },
+    { id: 11, name: "HONORS COLLEGE"},
+    { id: 9, name: "MECOP / CECOP" },
+    { id: 11, name: "MCNAIR"},
+    { id: 11, name: "EAGLES"},
     // Add more dummy programs as needed
   ];
 
@@ -16,20 +28,27 @@ function AdminPage() {
     { id: 1, name: "STEM" },
     { id: 2, name: "Engineering" },
     { id: 3, name: "Low Income" },
+    { id: 4, name: "Ada Disability" },
+    { id: 5, name: "Summer" },
+    { id: 6, name: "Latino / Hispanic" },
+    { id: 7, name: "Black / African American" },
+    { id: 8, name: "First Generation" },
+    { id: 9, name: "Native American" },
+    { id: 10, name: "Pacific Islander" },
     // Add more dummy tags as needed
   ];
 
   return (
     <div className="m-auto">
       {/* Intro section */}
-      <Row className="img-hero-welcome">
+      <Row className="img-hero-welcome p-3">
         {/*<Col className="hero-img"></Col>*/}
         <Col>
           <section>
             <h1 className="intro text-center fw-bold">
               Admin Tools
             </h1>
-            <p className = 'text-center'>Follow the instructions below to use the tool correctly..</p>
+            <p className = 'text-center'>Select a Program or Tag by clicking on it. You can only select one at a time!</p>
           </section>
         </Col>
       </Row>
@@ -39,9 +58,15 @@ function AdminPage() {
         <Col md={12} lg={6} className="" style={{ minHeight: "125px" }}>
           <div className="text-center border border-dark rounded p-5">
             <h2>Programs</h2>
-            {dummyPrograms.map((program) => (
-              <button key={program.id}>{program.name}</button>
-            ))}
+              {dummyPrograms.map((program) => (
+                  <button type="button" class="btn btn-primary p-3 m-1" key={program.id}>{program.name}</button>
+              ))}
+              <hr />
+              <div>
+                <button type="button" class="btn btn-dark  p-3 m-2">Add</button>
+                <button type="button" class="btn btn-dark p-3 m-2">Remove</button>
+                <button type="button" class="btn btn-dark p-3 m-2">Modify</button>
+              </div>
           </div>
         </Col>
 
@@ -49,10 +74,15 @@ function AdminPage() {
         <Col md={12} lg={6} className="" style={{ minHeight: "125px" }}>
           {/* You may replace this with your actual content */}
           <div className="text-center border border-dark rounded p-5">
-            <h2>Tags</h2>
             {dummyTags.map((tag) => (
-              <button key={tag.id}>{tag.name}</button>
+              <button type="button" class="btn btn-primary p-3 m-1" key={tag.id}>{tag.name}</button>
             ))}
+            <hr />
+            <div>
+              <button type="button" class="btn btn-dark  p-3 m-2">Add</button>
+              <button type="button" class="btn btn-dark p-3 m-2">Remove</button>
+              <button type="button" class="btn btn-dark p-3 m-2">Modify</button>
+            </div>
           </div>
         </Col>
       </Row>
@@ -66,24 +96,32 @@ function AdminPage() {
             <h2>ProgramInfo</h2>
             <form action="">
               <div>
-                <label htmlFor="ProgramName">Program Title</label>
+                <label htmlFor="ProgramName" className ="p-2">Program Title</label>
+                <br />
                 <input type="text" />
               </div>
               <div>
-                <label htmlFor="ProgramLeadContact">Lead Contact</label>
+                <label htmlFor="ProgramLeadContact" className="p-2">Lead Contact</label>
+                <br />
                 <input type="text" />
               </div>
               <div>
-                <label htmlFor="ProgramEmail">Contact Email</label>
+                <label htmlFor="ProgramEmail" className="p-2">Contact Email</label>
+                <br />
                 <input type="text" />
               </div>
               <div>
-                <label htmlFor="LinkToWeb">Link To Page</label>
+                <label htmlFor="LinkToWeb" className="p-2">Link To Page</label>
+                <br />
                 <input type="text" />
               </div>
               <div>
-                <label htmlFor="LongDescription">Long Description</label>
-                <input type="textarea" />
+                <label htmlFor="LongDescription" className="p-2">Long Description</label>
+                <br />
+                <textarea cols="45" rows="15"></textarea>
+              </div>
+              <div className="mt-3">
+                <input type="submit" class="btn btn-success p-3 m-2"/>
               </div>
             </form>
           </div>
@@ -95,12 +133,17 @@ function AdminPage() {
             <h2 className>Tag Info</h2>
             <form action="">
               <div>
-                <label htmlFor="TagName">Tag Name</label>
+                <label htmlFor="TagName" className="p-2">Tag Name</label>
+                <br />
                 <input type="text" />
               </div>
               <div>
-                <label htmlFor="TagCategory">Tag Category</label>
+                <label htmlFor="TagCategory" className="p-2">Tag Category</label>
+                <br />
                 <input type="text" />
+              </div>
+              <div className="mt-3">
+                <input type="submit" class="btn btn-success p-3 m-2"/>
               </div>
             </form>
           </div>
