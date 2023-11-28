0167a7f6c3a28ee7f93882a9edbcca0ca0fa091d
Anh Ho <anhho@pdx.edu>
2023-11-28 11:30:46 -0800

    able to create a new user in auth0, not showing on navbar yet - checking

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -2,8 +2,14 @@ import React from "react";
 //import logo from "./img/PSU_logo.png";
 import logo from "./image/PSU_logo.png"
 import "./CSS/NavBar.css";
+import { useAuth0 } from '@auth0/auth0-react';
 
 const NavBar = () => {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
+  const getUserInitials = (name) => {
+    return name.split(' ').map(part => part[0]).join('');
+  };
   return (
     <nav className="navbar navbar-expand-lg ">
       <div className="container-fluid text-dark">
@@ -52,6 +58,30 @@ const NavBar = () => {
               Admin Tools
             </a>
           </li>
+          <li className="nav-item">
+            {isAuthenticated ? (
+              <div className="d-flex align-items-center">
+                <div className="me-2">
+                  {user.picture ? (
+                    <img src={user.picture} alt="Profile" className="rounded-circle" style={{ width: '30px', height: '30px' }} />
+                  ) : (
+                    <span className="rounded-circle bg-primary text-white d-inline-flex justify-content-center align-items-center" style={{ width: '30px', height: '30px' }}>
+                      {getUserInitials(user.name)}
+                    </span>
+                  )}
+                </div>
+                <span>Welcome, {user.given_name || user.name}</span>
+                <button className="btn btn-outline-secondary ms-2" onClick={() => logout({ returnTo: window.location.origin })}>
+                  Log Out
+                </button>
+              </div>
+            ) : (
+              <button className="btn btn-outline-light" onClick={() => loginWithRedirect()}>
+                Log In
+              </button>
+            )}
+          </li>
+
         </ul>
       </div>
     </nav>
