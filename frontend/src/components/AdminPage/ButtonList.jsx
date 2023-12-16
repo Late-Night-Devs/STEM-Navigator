bb188af49b6969c78f2d5e18ba1acc374229ed55
Wes Wolf <wesleywwolf@gmail.com>
2023-12-16 12:17:41 -0800

    changed styles to use styled-components library. added CategoryColors containing all the custom colors used, and which Category of tag they are associated with.

--- frontend/src/components/AdminPage/ButtonList.jsx
+++ frontend/src/components/AdminPage/ButtonList.jsx
@@ -1,4 +1,58 @@
-import { React } from "react";
+import React from "react";
+import styled from "styled-components";
+
+const selectedButtonClass = "bg-warning";
+const CategoryColors = {
+  Eligibility: "#E0FAF",
+  Identity: "#ADE8F4",
+  ProgramFocus: "#ADE8F4",
+  StudentServices: "#48CAE4",
+  Timing: "#0096C7",
+  UniversityStatus: "#0077B6",
+  NoCategory: "#fb8500",
+};
+
+// StyledButton component for dynamic styling
+const StyledButton = styled.button`
+  padding: 0.25em 1em;
+  margin: 0.25em;
+  border-radius: 10px;
+  background-color: ${({ category }) => {
+    switch (category) {
+      case "Eligibility":
+        return CategoryColors.Eligibility;
+      case "Identity":
+      case "Identity ":
+        return CategoryColors.Identity;
+      case "Program Focus":
+        return CategoryColors.ProgramFocus;
+      case "Student Services":
+        return CategoryColors.StudentServices;
+      case "Timing":
+        return CategoryColors.Timing;
+      case "University Status":
+        return CategoryColors.UniversityStatus;
+      default:
+        return CategoryColors.NoCategory;
+    }
+  }};
+`;
+
+const AddButton = styled.button`
+  background-color: #6c757d; // btn-secondary
+  color: white;
+  padding: 0.25em 1em;
+  margin: 1em;
+  border-radius: 10px;
+`;
+
+const RemoveButton = styled.button`
+  background-color: #dc3545; // btn-danger
+  color: white;
+  padding: 0.25em 1em;
+  margin: 0.25em;
+  border-radius: 10px;
+`;
 
 export const ButtonList = ({
   name,
@@ -6,33 +60,6 @@ export const ButtonList = ({
   isItemSelected,
   handleButtonClick,
 }) => {
-  // Function to determine button color based on category, if it has one
-  const getButtonColor = (item) => {
-    // Check if the category property exists and is not undefined
-    const category = item.category;
-    if (category) {
-      const trimmedCategory = category.trim();
-      switch (trimmedCategory) {
-        case "Timing":
-          return "btn-light";
-        case "Identity":
-          return "btn-dark";
-        case "Student Services":
-          return "btn-secondary";
-        case "Eligibility":
-          return "btn-warning";
-        case "University Status":
-          return "btn-success";
-        case "Program Focus":
-          return "btn-info";
-        // Add more cases as needed
-        default:
-          return "btn-primary";
-      }
-    }
-    return "btn-primary"; // Default color for items without a category or unrecognized categories
-  };
-
   // Sort items by category, placing items without a category at the end
   const sortedItems = items.sort((a, b) => {
     if (!a.category) return 1;
@@ -42,27 +69,22 @@ export const ButtonList = ({
 
   return (
     <div className="mb-5" style={{ minHeight: "125px" }}>
-      <div className="text-center border border-dark rounded-5 p-1 m-3 ">
+      <div className="text-center border border-dark rounded-5 p-1 m-3">
         {sortedItems.map((item) => (
-          <button
+          <StyledButton
             type="button"
-            className={`btn p-2 m-1 ${getButtonColor(item)} ${
-              isItemSelected(item.id) ? "bg-info" : ""
-            }`}
+            category={item.category}
+            className={isItemSelected(item.id) ? selectedButtonClass : ""}
             key={item.id}
             onClick={() => handleButtonClick(item)}
           >
             {item.name}
-          </button>
+          </StyledButton>
         ))}
         <hr />
         <div>
-          <button type="button" className="btn btn-secondary p-2 m-2">
-            Add
-          </button>
-          <button type="button" className="btn btn-danger p-2 m-2">
-            Remove
-          </button>
+          <AddButton type="button">Add</AddButton>
+          <RemoveButton type="button">Remove</RemoveButton>
         </div>
       </div>
     </div>
