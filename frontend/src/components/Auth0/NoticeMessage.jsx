1a438885a72fbd50b9025f8e9cbe9554e5d6db4b
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-01-17 17:29:33 -0800

    Created: Alrert user to verify email before continuing

--- frontend/src/components/Auth0/NoticeMessage.jsx
+++ frontend/src/components/Auth0/NoticeMessage.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Modal from "react-modal";
+import VerifiedEmail from "../../image/verifyEmail.png"
+// Set the app element (main content element) for the modal
+Modal.setAppElement("#root");
 
 const LoginCheckMessage = () => {
   const { isAuthenticated } = useAuth0();
@@ -24,10 +28,7 @@ const LoginCheckMessage = () => {
   return (
     <div>
       <div
-        className={`fade ${
-          showVerificationNotice ? "show" : "hide"
-        }`}
-        // role="alert"
+        className={`fade ${showVerificationNotice ? "show" : "hide"}`}
         style={{
           backgroundColor: "#5C5394",
           color: "white",
@@ -42,8 +43,119 @@ const LoginCheckMessage = () => {
   );
 };
 
+
+const VerifiedEmailLogin = () => {
+  const { isAuthenticated, user, logout } = useAuth0();
+  const [countdown, setCountdown] = useState(30);
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+  let interval;
+  const checkEmailVerification = async () => {
+    if (isAuthenticated && user && !user.email_verified) {
+   
+      setModalIsOpen(true);
+
+      interval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+
+      // Countdown for automatic logout
+      if (countdown === 1) {
+        logout({ returnTo: window.location.origin });
+      }
+    } else {
+      // if email is verified. modal wont open.
+      setModalIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    checkEmailVerification();
+
+    // Clear the intervals on unmount or when the user confirms
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isAuthenticated, user, countdown]); // Include countdown in the dependency array
+
+  const handleConfirm = () => {
+    clearInterval(interval);
+    window.location.reload();
+  };
+
+  const handleLogout = () => {
+    setModalIsOpen(false);
+    logout({ returnTo: window.location.origin });
+  };
+
+  return (
+    <div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => {}}
+        shouldCloseOnOverlayClick={false}
+        shouldCloseOnEsc={false}
+        contentLabel="Verification Alert"
+        style={{
+          content: {
+            width: "80%",
+            margin: "20px auto",
+            padding: "2px",
+            border: "1px solid #5C5394",
+            boxShadow: "5px 4px 8px rgba(0, 0, 0.1, 0.1)",
+          },
+        }}
+      >
+        <div className="modal-content mx-0 ">
+          <div className="modal-body text-center mb-0">
+            <div className="">
+              <p
+                className="p-4 mb-0 rounded"
+                style={{
+                  backgroundColor: "#5C5394",
+                  color: "white",
+                  textAlign: "center",
+                  
+                }}
+              >
+                Please verify your email with Auth0 and refresh the page to
+                experience all the features this website has to offer. Hit "Confirm" when you're done!
+              </p>
+              <img
+                src={VerifiedEmail}
+                alt="Verification"
+                className="img-fluid mb-3 border mt-1 p-2 "
+                style={{ maxWidth: "80%", maxHeight: "80%" }}
+              />
+            </div>
+          </div>
+          <div className="modal-footer pt-2 gap-1 mx-auto mb-0">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleConfirm}
+            >
+              Confirm
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={handleLogout}
+            >
+              Logout in {countdown}s
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+
 const NoticeMessage = () => {
-  return <LoginCheckMessage />;
+  const { isAuthenticated } = useAuth0();
+  return (
+    <>{!isAuthenticated ? <LoginCheckMessage /> : <VerifiedEmailLogin />}</>
+  );
 };
 
 export default NoticeMessage;
