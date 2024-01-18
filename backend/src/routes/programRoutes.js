01beb4186d3777c3f4a85521bf4d85c6522c12e3
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-01-18 12:28:58 -0800

    Update programRoutes.js - Delete based on program_id

--- backend/src/routes/programRoutes.js
+++ backend/src/routes/programRoutes.js
@@ -6,12 +6,7 @@ const router = express.Router();
 router.get("/", programController.getPrograms);
 router.post("/filter", programController.filterPrograms);
 
-router.delete("/:programId", (req, res) => {
-  const programId = req.params.programId;
-  // logic to delete the program (and programTag entries)
-  res.send(
-    "backend recieved request to delete program with id " + programId.toString()
-  );
-});
+// delete all associated entries to proID and its self based on programId params
+router.delete("/:programId", programController.deleteProgram);
 
 module.exports = router;
