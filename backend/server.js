e55b8c0ce221ef73ec70a298a6dbd6fe1262ed6f
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-01-18 13:31:44 -0800

    UPDATED: Deletion for multi selected program ids - add some console.log to keep tracking

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,18 @@
 require('dotenv').config();
 // express server
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const path = require('path');
 const routes = require('./src/routes');
-const app = express();
+
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // path: http://backend_URL/${routes}
