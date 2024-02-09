c88d55209db25d8fbfe5e7119310575f7f9cfd09
Daniel Tran <danieltran219@gmail.com>
2024-02-09 02:26:34 -0800

    adding functionality to add or modify tags in admin page

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const checkAdminEmail = require("../adminAuthMiddleware");
+const { addTag, updateTag } = require("../controllers/tagController");
 
-router.get("/admin-modify-db", checkAdminEmail, (req, res) => {
+router.get("/admin-modify-db", (req, res) => {
   res.json({ message: "Welcome to the Admin Tools Page!" });
 });
 
 router.post("/admin-modify-db/tag-form-submit", (req, res) => {
-  console.log("backend recieved post: ", req.body);
-  res.send("backend recieved tag-form");
-  // if tag_id == -1 then the user wants to ADD a tag
-  // for any other tag id, the user wants to EDIT the tag
-})
+  const { tag_id } = req.body;
+
+  if (tag_id === -1) {
+    // Add a new tag
+    addTag(req, res);
+  } else {
+    // Update an existing tag
+    updateTag(req, res);
+  }
+});
 
 router.post("/admin-modify-db/program-form-submit", (req, res) => {
   console.log("backend recieved post: ", req.body);
   res.send("backend recieved program-form");
   // if program_id == -1 then the user wants to ADD a program
   // for any other program_id, the user wants to EDIT the program
-})
-
-
-
+});
 
 module.exports = router;
