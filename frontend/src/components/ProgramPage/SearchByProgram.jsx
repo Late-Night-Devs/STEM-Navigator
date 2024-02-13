a2b41c007436b135fed9f4cae5aa65cfbffc8a93
Matej Bozic <matej@pdx.edu>
2024-02-13 03:49:04 -0800

    Fixed error.
    Error: Duplicate form field id in the same form.

--- frontend/src/components/ProgramPage/SearchByProgram.jsx
+++ frontend/src/components/ProgramPage/SearchByProgram.jsx
@@ -6,12 +6,12 @@ function SearchByProgram(props) {
     <div className="search-top pt-3 pb-2">
       <div className="row g-2 align-items-center ">
         <div className="col-md-auto">
-          <label htmlFor="search-tag" className="search-label fw-bold fs-5"/>
+          <label htmlFor="search-program" className="search-label fw-bold fs-5"/>
         </div>
         <div className="col">
           <input
             className="rounded w-100 mx-md-2 mx-lg-0 search-for-a-program"
-            id="search-tag"
+            id="search-program"
             type="text"
             placeholder="Search for a program, e.g. ACCESS"
             onChange={(e) => props.handleSearchByProgram(e.target.value)}
