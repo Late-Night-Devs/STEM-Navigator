e7057fa19d4af0ba166d1622d2ab25d7623e2905
anhoop89 <102849461+anhoop89@users.noreply.github.com>
2024-02-18 14:03:39 -0800

    fixed: white space on the right when reducing the size to mobile vw

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
       <iframe
         title="Google Form"
         src="https://docs.google.com/forms/d/e/1FAIpQLSfGGXfHcg6imoAlJZhZAjLx8rjs7CfHvHv83px6grT7Kqz5uw/viewform?embedded=true"
-        width="640"
+        width="100%"
         height="1326"
         frameborder="0"
         marginheight="0"
